185510dea8a020a4b4bb63b03fdb9f0ac2ed4ccd
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-07-10 22:34:51 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,17 @@
-const db = require('./config/bd');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS, // debe coincidir con tu .env
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
 });
 
-db.connect((err) => {
+db.connect(err => {
   if (err) {
-    console.error('Error al conectar a MySQL:', err);
+    console.error('❌ Error al conectar a MySQL:', err);
     return;
   }
   console.log('🟢 Conexión a MySQL exitosa');
