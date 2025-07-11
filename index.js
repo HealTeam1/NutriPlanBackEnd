d7088ab52793f4aa015954eb53db240beaab1078
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-07-10 22:22:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2/promise');
+const connectDB = require('./config/db');
 
 const app = express();
 
 async function main() {
   try {
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      port: Number(process.env.DB_PORT),
-    });
-
-    console.log('Conectado a MySQL correctamente');
+    const connection = await connectDB();
 
     app.get('/', async (req, res) => {
       try {
