bc5f2b2f45c6fed5ef2c7009cf4e3929bd120a27
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-07-10 22:25:45 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS, // asegúrate que coincida con tu .env
+  password: process.env.DB_PASS, // debe coincidir con tu .env
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT), // si tienes puerto personalizado
+  port: Number(process.env.DB_PORT),
 });
 
 db.connect((err) => {
