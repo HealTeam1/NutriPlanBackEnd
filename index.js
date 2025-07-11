6e73f088a80bd0e38ff46ebd9f87b81d39147db5
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-07-10 22:12:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const app = express();
 
-  const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-});
+async function main() {
+  try {
+    const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
-connection.connect(err => {
-  if (err) {
+    console.log('Conectado a MySQL correctamente');
+
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Servidor corriendo en puerto ${process.env.PORT || 3000}`);
+    });
+
+  } catch (err) {
     console.error('Error al conectar a MySQL:', err);
-    return;
   }
-  console.log('Conectado a MySQL correctamente');
-});
+}
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Servidor corriendo en puerto ${process.env.PORT || 3000}`);
-});
+main();
