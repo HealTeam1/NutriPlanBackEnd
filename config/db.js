97eaeb81cc5a7ea2f4ffd891844fefb1c9c5c67d
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-07-10 22:57:33 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,20 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST,       // mainline.proxy.rlwy.net
+  port: process.env.DB_PORT,       // 24706
+  user: process.env.DB_USER,       // root
+  password: process.env.DB_PASS,   // EJqkGGhzELxafamBksrTUfXSsepulXvr
+  database: process.env.DB_NAME    // railway
 });
 
 connection.connect((err) => {
   if (err) {
     console.error('❌ Error al conectar a MySQL:', err);
-    return;
+    process.exit(1);  // opcional: termina si no hay conexión
+  } else {
+    console.log('✅ Conectado a MySQL con éxito');
   }
-  console.log('✅ Conectado a MySQL con éxito');
 });
+
+module.exports = connection;
