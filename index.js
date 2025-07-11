a93ebf4686b3bb9e14a5c4dff0ff7277bcbe69d9
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-07-10 22:08:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const mysql = require('mysql2');
 
 const app = express();
 
-const connection = mysql.createConnection({
+  const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
 });
 
 connection.connect(err => {
