fc6188f26e99804fc28ad8179245352025479d57
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-07-10 22:13:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const app = express();
 
 async function main() {
   try {
-    const connection = await mysql.createConnection(process.env.DATABASE_URL);
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT),
+    });
 
     console.log('Conectado a MySQL correctamente');
-
 
     app.listen(process.env.PORT || 3000, () => {
       console.log(`Servidor corriendo en puerto ${process.env.PORT || 3000}`);
